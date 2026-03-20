cbdd5f35e881b24938000ef04636dc4f5f26bc84
fredchan23 <fredckp@gmail.com>
2026-03-20 22:24:07 +0800

    Add new dimensional and fact tables for Olist data mart
    - Created `dim_customers` table to store customer details.
    - Created `dim_date` table for date dimensions.
    - Created `dim_products` table for product information.
    - Created `dim_sellers` table for seller details.
    - Created `fact_reviews` table to capture review data.
    - Created `fact_order_items` table for order item details.
    - Created `fact_orders` table to aggregate order information.
    - Created `mart_customer_rfm_raw` table for customer RFM analysis.
    - Created `mart_customer_rfm_scored` table to score customers based on RFM metrics.
    - Created `mart_delivery_performance` table to analyze delivery performance.
    - Created `mart_monthly_sales` table for monthly sales metrics.
    - Created `mart_seller_performance` table to evaluate seller performance.
    - Created `mart_top_products` table to identify top-selling products.

---
+++ olist/target/compiled/olist/models/dim/dim_sellers.sql
@@ -0,0 +1,13 @@
+with __dbt__cte__src_sellers as (
+select
+    cast("seller_id" as varchar(32)) as seller_id,
+    cast("seller_zip_code_prefix" as varchar(5)) as seller_zip_code_prefix,
+    trim("seller_city") as seller_city,
+    trim("seller_state") as seller_state
+from OLIST.RAW.raw_sellers
+) select
+    seller_id,
+    seller_zip_code_prefix,
+    seller_city,
+    seller_state
+from __dbt__cte__src_sellers
