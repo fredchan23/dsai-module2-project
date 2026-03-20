065930991a9962d69e7ae7ff07a6d2fc8182cdae
fredchan23 <fredckp@gmail.com>
2026-03-20 15:42:46 +0800

    cleaned target folder

--- olist/target/compiled/olist/models/src/src_order_reviews.sql
+++
@@ -1,9 +0,0 @@
-select
-    cast("review_id" as varchar(32)) as review_id,
-    cast("order_id" as varchar(32)) as order_id,
-    cast("review_score" as number(2, 0)) as review_score,
-    nullif(trim("review_comment_title"), '') as review_comment_title,
-    nullif(trim("review_comment_message"), '') as review_comment_message,
-    cast("review_creation_date" as timestamp_ntz) as review_creation_ts,
-    cast("review_answer_timestamp" as timestamp_ntz) as review_answer_ts
-from OLIST.RAW.raw_order_reviews
